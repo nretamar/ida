3e99b72acfe4eb41c9e3fa96dd5d8d98ff177a20
Tomas <tomasgomila@hotmail.com>
2018-10-08 00:59:06 -0300

    agregado entities a util

--- Servidor_Login/src/util/HibernateUtil.java
+++ Servidor_Login/src/util/HibernateUtil.java
@@ -4,9 +4,13 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 
 import entity.ClienteEntity;
+import entity.OrdenDeCompraEntity;
+import entity.OrdenRecepcionItemEntity;
 import entity.PedidoEntity;
 import entity.PedidoItemEntity;
 import entity.ProductoEntity;
+import entity.RemitoEntity;
+import entity.RemitoItemEntity;
 
 public class HibernateUtil
 {
@@ -18,9 +22,13 @@ public class HibernateUtil
         {
         	 AnnotationConfiguration config = new AnnotationConfiguration();
              config.addAnnotatedClass(ClienteEntity.class);
-             config.addAnnotatedClass(ProductoEntity.class);
+             config.addAnnotatedClass(OrdenDeCompraEntity.class);
+             config.addAnnotatedClass(OrdenRecepcionItemEntity.class);
              config.addAnnotatedClass(PedidoEntity.class);
              config.addAnnotatedClass(PedidoItemEntity.class);
+             config.addAnnotatedClass(ProductoEntity.class);
+             config.addAnnotatedClass(RemitoEntity.class);
+             config.addAnnotatedClass(RemitoItemEntity.class);
              
              sessionFactory = config.buildSessionFactory();
         }

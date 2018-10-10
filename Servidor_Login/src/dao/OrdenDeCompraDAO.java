033598483480ab08fd94b460f82e5f909246024b
Tomas <tomasgomila@hotmail.com>
2018-10-10 02:29:52 -0300

    Queda pendiente realizar

---
+++ Servidor_Login/src/dao/OrdenDeCompraDAO.java
@@ -0,0 +1,18 @@
+package dao;
+
+public class OrdenDeCompraDAO {
+	
+	private static OrdenDeCompraDAO instancia;
+
+	private OrdenDeCompraDAO() {
+	}
+
+	public static OrdenDeCompraDAO getInstance() {
+		if (instancia == null)
+			instancia = new OrdenDeCompraDAO();
+		return instancia;
+	}
+	
+	//TODO
+	
+}

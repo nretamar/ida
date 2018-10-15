b555bbbb52bb3e231dcf02c8e2a3e221ff59383e
Tomas <tomasgomila@hotmail.com>
2018-10-15 03:16:06 -0300

    Cambios

--- Repositorio_Login/src/remoteInterfaz/ExpedicionRemote.java
+++ Repositorio_Login/src/remoteInterfaz/ExpedicionRemote.java
@@ -9,14 +9,30 @@ import dto.PedidoDTO;
 
 public interface ExpedicionRemote extends Remote, Serializable {
 	
+	List<PedidoDTO> findAllPedidos() throws RemoteException;
+	
 	Integer altaPedido(PedidoDTO pedido) throws RemoteException;
 
-	void despacharPedido(Integer pedidoId) throws RemoteException;
-
-	void cancelarPedido(Integer pedidoId) throws RemoteException;
-
-	PedidoDTO buscarPedido(Integer pedidoId) throws RemoteException;
-
+	void actualizarTodoFaltaDeStockAPendiente() throws RemoteException;
+	
+	PedidoDTO pendienteSiTengoStock(Integer idPedido) throws RemoteException;
+	
+	boolean tengoStock(Integer idPedido) throws RemoteException;
+	
+	boolean bajaPedido(Integer idPedido) throws RemoteException;
+	
+	void modificarPedido(PedidoDTO pedido) throws RemoteException;
+	
+	PedidoDTO buscarPedido(Integer idPedido) throws RemoteException;
+	
 	List<PedidoDTO> buscarPedidosPendientesDespacho() throws RemoteException;
+	
+	void cancelar(Integer idPedido) throws RemoteException;
+	
+	void despachar(Integer idPedido) throws RemoteException;
+	
+	PedidoDTO buscarPedidoByDireccionEnvioCoordinado(String direccion) throws RemoteException;
+
+	int buscarFaltaStockByProducto(String codigoBarras) throws RemoteException;
 	
 }
--- Servidor_Login/src/remoteImplementation/ExpedicionRemoteImpl.java
+++ Servidor_Login/src/remoteImplementation/ExpedicionRemoteImpl.java
@@ -17,19 +17,38 @@ public class ExpedicionRemoteImpl extends UnicastRemoteObject implements Expedic
 	}
 
 	@Override
+	public List<PedidoDTO> findAllPedidos() throws RemoteException {
+		return ExpedicionControlador.getInstancia().findAllPedidos();
+	}
+
+	@Override
 	public Integer altaPedido(PedidoDTO pedido) throws RemoteException {
 		return ExpedicionControlador.getInstancia().altaPedido(pedido);
 	}
 
 	@Override
-	public void despacharPedido(Integer idPedido) throws RemoteException {
-		ExpedicionControlador.getInstancia().despachar(idPedido);
-		
+	public void actualizarTodoFaltaDeStockAPendiente() throws RemoteException {
+		ExpedicionControlador.getInstancia().actualizarTodoFaltaDeStockAPendiente();
 	}
 
 	@Override
-	public void cancelarPedido(Integer idPedido) throws RemoteException {
-		ExpedicionControlador.getInstancia().cancelar(idPedido);
+	public PedidoDTO pendienteSiTengoStock(Integer idPedido) throws RemoteException {
+		return ExpedicionControlador.getInstancia().pendienteSiTengoStock(idPedido);
+	}
+
+	@Override
+	public boolean tengoStock(Integer idPedido) throws RemoteException {
+		return ExpedicionControlador.getInstancia().tengoStock(idPedido);
+	}
+
+	@Override
+	public boolean bajaPedido(Integer idPedido) throws RemoteException {
+		return ExpedicionControlador.getInstancia().bajaPedido(idPedido);
+	}
+
+	@Override
+	public void modificarPedido(PedidoDTO pedido) throws RemoteException {
+		ExpedicionControlador.getInstancia().modificarPedido(pedido);
 	}
 
 	@Override
@@ -40,6 +59,26 @@ public class ExpedicionRemoteImpl extends UnicastRemoteObject implements Expedic
 	@Override
 	public List<PedidoDTO> buscarPedidosPendientesDespacho() throws RemoteException {
 		return ExpedicionControlador.getInstancia().buscarPedidosPendientesDespacho();
+	}
+
+	@Override
+	public void cancelar(Integer idPedido) throws RemoteException {
+		ExpedicionControlador.getInstancia().cancelar(idPedido);
+	}
+
+	@Override
+	public void despachar(Integer idPedido) throws RemoteException {
+		ExpedicionControlador.getInstancia().despachar(idPedido);
+	}
+
+	@Override
+	public PedidoDTO buscarPedidoByDireccionEnvioCoordinado(String direccion) throws RemoteException {
+		return ExpedicionControlador.getInstancia().buscarPedidoByDireccionEnvioCoordinado(direccion);
+	}
+
+	@Override
+	public int buscarFaltaStockByProducto(String codigoBarras) throws RemoteException {
+		return ExpedicionControlador.getInstancia().buscarFaltaStockByProducto(codigoBarras);
 	}
 
 }
